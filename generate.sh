2af11b80c0ff06419de06b7f9508e3ba6358d99f
shivasdst <shivably04sdst@gmail.com>
2018-01-06 19:21:58 +0530

    utf8 encoding is added

--- generate.sh
+++ generate.sh
@@ -5,6 +5,6 @@ db="igipa"
 usr="root"
 pwd='mysql'
 
-echo "drop database if exists $db; create database $db;" | /usr/bin/mysql -uroot -p$pwd
+echo "drop database if exists $db; create database $db CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;" | /usr/bin/mysql -uroot -p$pwd
 perl insert_progs.pl $host $db $usr $pwd
 perl insert_kagga.pl $host $db $usr $pwd
