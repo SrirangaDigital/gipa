6efa78d8550ce562df8bcd6babecd72dfc76361e
shivasdst <shivably04sdst@gmail.com>
2018-01-06 19:27:31 +0530

    trial

--- php/connect.php
+++ php/connect.php
@@ -5,6 +5,7 @@
 	$database = 'igipa';
 		
 	$mysqli = new mysqli("$host","$user","$password", "$database");
+	$mysqli->set_charset("utf8");
 	if ($mysqli->connect_errno) {
 		echo "Errno: " . $mysqli->connect_errno . "\n";
 		echo "Error: " . $mysqli->connect_error . "\n";
