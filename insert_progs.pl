9de9650aeaebaead7bf286fd70c19f783a3ada41
shivasdst <shivably04sdst@gmail.com>
2018-01-06 19:23:58 +0530

    removed mysql_enable_utf8

--- insert_progs.pl
+++ insert_progs.pl
@@ -10,7 +10,7 @@ use DBI();
 open(IN,"gipa.xml") or die "can't open gipa.xml\n";
 
 my $dbh=DBI->connect("DBI:mysql:database=$db;host=$host","$usr","$pwd");
-$dbh->{'mysql_enable_utf8'} = 1;
+#$dbh->{'mysql_enable_utf8'} = 1;
 $dbh->do('SET NAMES utf8');
 
 
