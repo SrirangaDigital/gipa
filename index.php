dcf4e04bbeb68ac5ca343a5f9f05f599288968fa
shivasdst <shivably04sdst@gmail.com>
2019-01-19 19:30:46 +0530

    home page updated

--- index.php
+++ index.php
@@ -125,6 +125,11 @@ if($num_rows4)
 }
 ?>
 			</table>
+			<div class="newswithborder">
+				<p class="flash-news">ಡಿ.ವಿ. ಗುಂಡಪ್ಪರವರ ಎಲ್ಲ ಪುಸ್ತಕಗಳು ಇ-ಬುಕ್ಸ್ ಆಗಿ ಗೂಗಲ್ ಪ್ಲೇ ಬುಕ್ಸ್‌ನಲ್ಲಿ ಲಭ್ಯವಿದೆ.</p>
+				<p class="flash-news">All Kannada books of D.V. Gundappa are available as e-Books on<br /> Google Play Books.<br /><br /></p>
+				<p class="flash-news"><img class="playbooksicon" src="php/images/google-play-books.png" alt="Google Play Books Logo" title="Google Play Books"/> Link: <a class="bld_color" href="http://sriranga.digital/dvg" target="_blank">sriranga.digital/dvg</a></p>
+			</div>	
 			<div class="databottom">
 				<div class="kagga">
 				<?php
