ff42cfa4886fee13055d1b13587a3da3fbd2bcc1
Sriraghunandana92 <raghunandanakeremane@gmail.com>
2024-06-05 16:55:44 +0530

    2 tables added

--- generate.sh
+++ generate.sh
@@ -3,8 +3,10 @@
 host="localhost"
 db="igipa"
 usr="root"
-pwd='mysql'
+pwd='Pass246!'
 
 echo "drop database if exists $db; create database $db CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;" | /usr/bin/mysql -uroot -p$pwd
 perl insert_progs.pl $host $db $usr $pwd
 perl insert_kagga.pl $host $db $usr $pwd
+perl insert_artist.pl $host $db $usr $pwd
+perl insert_tracks.pl $host $db $usr $pwd
